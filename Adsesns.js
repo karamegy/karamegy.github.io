7ccfbac8fc4d3078e5dd18a09180bad201c9cf30
karamegy <79487897+karamegy@users.noreply.github.com>
2026-09-20 03:55:23 +0300

    Integrate AdSense script and ad container
    Add AdSense script and ad container to the project.

---
+++ Adsesns.js
@@ -0,0 +1,23 @@
+إضافة السكريبت الرئيسي في هيدر المشروع الجديد (index.html):
+ضع هذا السطر بين علامتي <head> و </head> في الملف الرئيسي للمشروع الجديد:
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6651309355082807" crossorigin="anonymous"></script>
+
+
+2 وضع حاوية الإعلان في مكان العرض داخل الصفحة (<body>):
+ض
+
+
+<div class="ad-container">
+  <ins class="adsbygoogle"
+       style="display:block"
+       data-ad-client="ca-pub-6651309355082807"
+       data-ad-slot="ضع_رقم_الوحدة_الجديد_هنا"
+       data-ad-format="auto"
+       data-full-width-responsive="true"></ins>
+  <script>
+       (adsbygoogle = window.adsbygoogle || []).push({});
+  </script>
+</div>
+
+
+هذا معرف اد سينس يتم وضعخا في اي حساب يخصني
